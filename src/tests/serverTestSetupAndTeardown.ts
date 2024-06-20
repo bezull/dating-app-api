9b4471db6c1c2388b0ca4ec1c2cc0eae9a6e4bbe
Dzulfikar Dzulfikar <kardzulfi1805@gmail.com>
2024-06-20 14:31:54 +0700

    chore: setup e2e test
    * Added jest cucumber and axios dependencies

--- src/tests/serverTestSetupAndTeardown.ts
+++ src/tests/serverTestSetupAndTeardown.ts
@@ -1,7 +1,31 @@
 import { sequelizeConnection } from '../shared/infra/database'
+import { WebServer } from '../shared/infra/http/webServer'
 
 const purgeDatabase = async () => {
   await sequelizeConnection.sequelize.sync({ force: true })
+}
+
+export const startServer = async (webServer: WebServer): Promise<WebServer> => {
+  return new Promise((resolve, reject) => {
+    try {
+      webServer.start()
+      resolve(webServer)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+export const stopServer = async (webServer: WebServer): Promise<WebServer> => {
+  return new Promise((resolve, reject) => {
+    if (webServer) {
+      webServer.stop()
+      if (!webServer.isStarted) {
+        return resolve(webServer)
+      }
+      return reject('Error when stopping server')
+    }
+  })
 }
 
 export const infraSetupAndTeardown = () => {
